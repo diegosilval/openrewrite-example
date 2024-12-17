437fb15888aa54db399bee8941becfcf21d05348
Diego Silva <diego.silval@gmail.com>
2024-12-17 09:00:09 -0500

    Nuevas declaraciones

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ```shell
 mvn -U org.openrewrite.maven:rewrite-maven-plugin:run \
     -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-migrate-java:RELEASE \
-    -Drewrite.activeRecipes=org.openrewrite.java.migrate.lang.UseVar \
+    -Drewrite.activeRecipes=com.sample.MyRecipe \
     -Drewrite.exportDatatables=true
 ```
--- src/main/java/com/apuntesdejava/openrewrite/example/OpenrewriteExample.java
+++ src/main/java/com/apuntesdejava/openrewrite/example/OpenrewriteExample.java
@@ -1,12 +1,40 @@
 package com.apuntesdejava.openrewrite.example;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class OpenrewriteExample {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         KeyFactory factory = KeyFactory.getInstance("EC");
         StringBuilder sb = new StringBuilder("A dummy text");
+        KeyFactory factory0 = (KeyFactory) KeyFactory.getInstance("EC");
+        AClass aObject = new AClass();
+        KeyFactory factory1 = factory;
+        BClass bObject = BClass.getInstance();
+        List<String> list = List.of("one", "two", "three", "four");
+        Path path = Path.of("debug.log");
+        InputStream stream = Files.newInputStream(path);
+
+    }
+}
+
+class AClass {
+}
+
+class BClass {
+    private static BClass INSTANCE;
+
+    private BClass() {
+    }
+
+    public static BClass getInstance() {
+        return INSTANCE == null ? INSTANCE = new BClass() : INSTANCE;
+
     }
 }
